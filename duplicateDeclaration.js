2c9eb886a5dfb5f24e03599d6a73c60f2b44ea44
Mingzhang Yang <mingzhang.yang@nih.gov>
2018-04-09 12:14:31 -0400

    add comments

--- duplicateDeclaration.js
+++ duplicateDeclaration.js
@@ -5,7 +5,7 @@
 
 function foo(a, b, c) {
   console.log(a);
-  var b, c;
+  var b, c; // duplicate declaration. It will not pass compilation in C/C++, Go.
   //let c; // This is another reason you should use 'let' instead of 'var'
   console.log(b);
   console.log(c);
