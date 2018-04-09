0aec59fc2630089d6ebc375ce9dc278aeddd9c63
Mingzhang Yang <mingzhang.yang@nih.gov>
2018-04-09 11:59:01 -0400

    add what?

---
+++ add.js
@@ -0,0 +1,14 @@
+/**
+ * Created by yangm11 on 4/9/2018.
+ */
+'use strict';
+
+console.log('[3] + [4] = ', [3] + [4]);
+console.log('(1, 2, 3) + (4, 5, 6) = ', (1, 2, 3) + (2, 4, 6));
+console.log('Array(1) + Array(2) = ', Array(1) + Array(2));
+
+if (typeof module !== 'undefined' && module.parent) {
+
+} else {
+  // test code go here
+}
